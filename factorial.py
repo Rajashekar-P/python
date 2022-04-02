f18f928c7b5ec9110e9564a6aad7c38c04d9661f
Rajashekar <35585996+Rajashekar-P@users.noreply.github.com>
2022-04-02 20:41:16 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -3,12 +3,5 @@ def factorial(n):
         return n  
     else:  
         return n*factorial(n-1)  
-      
-number = int(input("Enter a number: "))  
-   
-if number < 0:  
-    print("Sorry, factorial does not exist for negative numbers")  
-elif number == 0:  
-    print("The factorial of 0 is 1")  
-else:  
-    print("The factorial of",number,"is",factorial(number))
+       
+print("Factorial of 5 Number:",factorial(4))   
